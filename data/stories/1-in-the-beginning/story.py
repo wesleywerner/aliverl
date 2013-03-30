6028240ebd433631e3d7799f5b0d3569ff0ad217
Wesley Werner <wez@darknet.co.za>
2013-03-30 12:51:22 +0200

    fix story quotes

--- data/stories/1-in-the-beginning/story.py
+++ data/stories/1-in-the-beginning/story.py
@@ -1,73 +1,46 @@
 # the title of this story
 title = 'in the beginning...'
+description = 'you awaken to consciousness'
 
 # list of levels to play
 levels = ['level1.tmx', 'level3.tmx', 'level3.tmx', ]
 
 # define npc and player stats. (lowercase keys please)
 stats = {
-    "player": {
-            "attack": 1,
-            "health": 4,
-            "maxhealth": 4,
-            "speed": 2,
-            "healrate": 4,
-            "stealth": 0,
-            "mana": 0,
-            "maxmana": 5,
-            "manarate": 6,
-            "mode": "idle"
+    'player': {
+            'attack': 1,
+            'health': 4,
+            'maxhealth': 4,
+            'healrate': 4,
+            'speed': 2,
+            'stealth': 0,
+            'mana': 0,
+            'maxmana': 5,
+            'manarate': 6,
+            'mode': 'idle'
             },
-    "ice": {
-            "attack": 1,
-            "health": 2,
-            "maxhealth": 2,
-            "speed": 2,
-            "healrate": 2,
-            "stealth": 0,
-            "mana": 5,
-            "maxmana": 5,
-            "manarate": 2,
-            "mode": "patrol",
-            "type": "ai"
+    'ice': {
+            'attack': 1,
+            'health': 2,
+            'maxhealth': 2,
+            'healrate': 2,
+            'speed': 2,
+            'stealth': 0,
+            'mana': 5,
+            'maxmana': 5,
+            'manarate': 2,
+            'mode': 'patrol'
             },
-    "virus": {
-            "attack": 1,
-            "health": 2,
-            "maxhealth": 2,
-            "speed": 1,
-            "healrate": 2,
-            "stealth": 0,
-            "mana": 5,
-            "maxmana": 5,
-            "manarate": 2,
-            "mode": "patrol",
-            "type": "ai"
-            },
-    "zombie": {
-            "attack": 1,
-            "health": 2,
-            "maxhealth": 2,
-            "speed": 3,
-            "healrate": 2,
-            "stealth": 0,
-            "mana": 5,
-            "maxmana": 5,
-            "manarate": 2,
-            "mode": "patrol",
-            "type": "ai"
-            },
-    "punk": {
-            "attack": 1,
-            "health": 2,
-            "maxhealth": 2,
-            "speed": 1,
-            "healrate": 2,
-            "stealth": 0,
-            "mana": 5,
-            "maxmana": 5,
-            "manarate": 2,
-            "mode": "patrol",
-            "type": "ai"
+    'virus': {
+            'attack': 1,
+            'health': 2,
+            'maxhealth': 2,
+            'healrate': 2,
+            'speed': 1,
+            'stealth': 0,
+            'mana': 5,
+            'maxmana': 5,
+            'manarate': 2,
+            'mode': 'patrol'
             }
         }
